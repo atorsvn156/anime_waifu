690f67e6bdcf52a9b6276de2290e9679d98a4359
atorsvn156 <98623999+atorsvn156@users.noreply.github.com>
2022-05-25 18:28:17 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-sudo apt-get update
-sudo apt-get -y install sox
-sudo apt-get install ffmpeg
-pip3 install git+https://github.com/Rapptz/discord.py
+install-pkg sox
+install-pkg ffmpeg
 pip3 install -r requirements.txt
